e59f28d713333449671e0080a61d3f4dac8bd650
GrantE2003 <116228933+GrantE2003@users.noreply.github.com>
2024-03-03 21:22:17 -0600

    Added Comments

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,12 @@
-#Test
+# I have done this project before... so while this code is original, I only
+# have this single branch since it only took me one attempt.
+
+#For Loop
+
+    # Check if number "i" is a multiple of 3 and 5
+    
+    # Check to see if "i" is multiple of 3
+    
+    # Check to see if "i" is a multiple of 5
+    
+    # Print "i" if it is not a multiple of 3 or 5
