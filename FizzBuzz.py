1cdcd6f9ddfc974a7fc3979986fe9456f33858a6
GrantE2003 <116228933+GrantE2003@users.noreply.github.com>
2024-03-03 21:24:00 -0600

    Added "for" loop logic

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,11 +2,11 @@
 # have this single branch since it only took me one attempt.
 
 #For Loop
-
+for i in range(1,101):
     # Check if number "i" is a multiple of 3 and 5
     
     # Check to see if "i" is multiple of 3
     
     # Check to see if "i" is a multiple of 5
-    
+
     # Print "i" if it is not a multiple of 3 or 5
