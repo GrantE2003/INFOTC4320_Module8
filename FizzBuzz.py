5b91245a08dc50164f73f670f3099ca8a38377ab
GrantE2003 <116228933+GrantE2003@users.noreply.github.com>
2024-03-03 21:34:37 -0600

    Changed "if" statements for Fizz and Buzz to "elif" so code functions correctly.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -8,11 +8,11 @@ for i in range(1,101):
         # Print FizzBuzz if "i" is a multiple of 3 and 5
         print("FizzBuzz")
     # Check to see if "i" is multiple of 3
-    if ((i % 3) == 0):
+    elif ((i % 3) == 0):
         # Print Fizz if "i" is a multiple of 3
         print("Fizz")
     # Check to see if "i" is a multiple of 5
-    if ((i % 5) == 0):
+    elif ((i % 5) == 0):
         # Print Buzz if "i" is a multiple of 5
         print("Buzz")
     # Check if number "i" is not a multiple of 3 or 5
